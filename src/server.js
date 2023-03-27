b2996c7968bb15d85377ddc6215a33b55980d4c9
Billy Franscois <BillyFranscois@outlook.com>
2023-03-28 00:56:31 +0900

    Remove comment's host

--- src/server.js
+++ src/server.js
@@ -6,7 +6,6 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    // host: 'localhost',
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
